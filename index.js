c0b891101cb82304570fad5273f7b9d6252f497d
Jose Marquez <brea820@gmail.com>
2025-05-27 17:01:24 -0400

    osoo

--- index.js
+++ index.js
@@ -33,4 +33,18 @@ app.use("/api/v1/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en PUERTO: ${PORT}`);
+
+  // Cada 4 segundos, hacer petición GET a la URL externa
+  setInterval(async () => {
+    try {
+      const response = await fetch("https://backend-gradus-n2nm.onrender.com");
+      if (response.ok) {
+        console.log("Ping exitoso");
+      } else {
+        console.log("Ping falló con status:", response.status);
+      }
+    } catch (error) {
+      console.error("Error haciendo ping", error.message);
+    }
+  }, 4000);
 });
